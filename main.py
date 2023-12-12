1895221375da568640e6329eebf4d5586eeb9444
io-23SklyarAnton <80kap.i.toshka@gmail.com>
2023-12-13 00:08:39 +0200

    add func

--- main.py
+++ main.py
@@ -3,3 +3,5 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+def add(a, b):
+    return a + b
